eaadc0196399d53f5fa288c18a2565acfc6d3aea
AlexJYad <alexanderjacovlev@gmail.com>
2024-07-04 12:35:14 +0200

    feature: mobile v2.2

--- README.md
+++ README.md
@@ -3,19 +3,19 @@
 Сверстайте адаптивный лендинг [по макету в Figma](https://www.figma.com/file/0xXfupPNU3aZxPqFbmhCKb/%D0%94%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD-%D0%B4%D0%BB%D1%8F-%D0%B2%D0%B5%D1%80%D1%81%D1%82%D0%BA%D0%B8-%7C-%D0%A2%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D1%8B%D0%B9-%D0%BB%D0%B5%D0%BD%D0%B4%D0%B8%D0%BD%D0%B3?type=design&node-id=0%3A1&mode=design&t=rOAaagCJbi3KLDi4-1), используя стек html + css + чистый js (без библиотек и фреймворков).
 
 - [x] Доделать a:hover
-- [ ] Телефонную версию
-  - [x] main
-  - [ ] section 1
-  - [ ] section 2
-  - [ ] section 3
-  - [ ] footer
-- [ ] Вывреку размеров
+- [x] Телефонную версию
   - [x] main
   - [x] section 1
   - [x] section 2
   - [x] section 3
   - [x] footer
-- [ ] Картинки
+- [x] Вывреку размеров
+  - [x] main
+  - [x] section 1
+  - [x] section 2
+  - [x] section 3
+  - [x] footer
+- [x] Картинки
   - [x] main
   - [x] photo
   - [x] collage
@@ -24,3 +24,5 @@
 - [ ] Улучшить CSS
   - [ ] подправить классы
   - [ ] подправить повтораймость
+- [ ] Улучшить позиционирование изображений
+  - [ ] Добавить кручение диска
--- script.js
+++ script.js
@@ -4,10 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
    const prevButton = document.querySelector(".prev");
    const nextButton = document.querySelector(".next");
    const counter = document.querySelector(".counter");
+   const pagination = document.querySelector(".carousel-pagination");
 
    const slideWidth = carousel.querySelector(".slide").offsetWidth;
    let currentIndex = 0;
    const totalSlides = slides.children.length;
+
+   const track = document.querySelector(".carousel-track");
+   const slide0 = document.querySelector(".grid-1");
+   const slides2 = Array.from(track.children);
+   const nextButton2 = document.querySelector(".carousel-button.next2");
+   const prevButton2 = document.querySelector(".carousel-button.prev2");
+   prevButton2.disabled = true;
+   const slideWidth2 = slide0.getBoundingClientRect().width + 50; //slides2[0].getBoundingClientRect().width;
+
+   let currentSlide = 0;
 
    function updateCounter() {
       counter.textContent = `${currentIndex + 1} / ${totalSlides}`;
@@ -51,4 +62,52 @@ document.addEventListener("DOMContentLoaded", function () {
 
    updateCounter();
    updateButtonState();
+
+   function updateButtonState2() {
+      if (currentSlide === 0) {
+         prevButton2.disabled = true;
+      } else {
+         prevButton2.disabled = false;
+      }
+      if (currentSlide === 4) {
+         nextButton2.disabled = true;
+      } else {
+         nextButton2.disabled = false;
+      }
+   }
+
+   [0, 1, 2, 3, 4].forEach((slide, index) => {
+      const button = document.createElement("button");
+      if (index === 0) button.classList.add("active");
+      button.addEventListener("click", () => {
+         currentSlide = index;
+         updateCarousel();
+      });
+      pagination.appendChild(button);
+   });
+
+   const updateCarousel = () => {
+      track.style.transform =
+         "translateX(-" + slideWidth2 * currentSlide + "px)";
+      document
+         .querySelectorAll(".carousel-pagination button")
+         .forEach((button, index) => {
+            button.classList.toggle("active", index === currentSlide);
+         });
+      updateButtonState2();
+   };
+
+   nextButton2.addEventListener("click", () => {
+      if (currentSlide < 4) {
+         currentSlide++;
+         updateCarousel();
+      }
+   });
+
+   prevButton2.addEventListener("click", () => {
+      if (currentSlide > 0) {
+         currentSlide--;
+         updateCarousel();
+      }
+   });
 });
